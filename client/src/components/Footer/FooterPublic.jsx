321f63330186d02d1f4171e3ff1a9c624113f314
Javier Sanz <javiersanzvilloldo@gmail.com>
2026-01-21 20:25:42 +0100

    actu

--- client/src/components/Footer/FooterPublic.jsx
+++ client/src/components/Footer/FooterPublic.jsx
@@ -1,5 +1,4 @@
-import "./footer.css"
-
+import "./footer.css";
 import {
   FaMapMarkerAlt,
   FaPhoneAlt,
@@ -7,87 +6,59 @@ import {
   FaInstagram,
   FaWhatsapp,
   FaEnvelope,
-} from 'react-icons/fa';
-
+} from "react-icons/fa";
 
 export const FooterPublic = () => {
   return (
-    <div>
-      <footer className="footer">
-      
-        <div className="footer__content">
-           {/* Logo */}
-          <div className="footer__logo">
-            
-            <img src="/logo-bruver.png" alt="Brüver - Fieles a tu mascota" />
-            
-          </div>
-          {/* Información */}
-          <div className="footer__info">
-                       <h4>Dónde estamos</h4>      
-            <p>
-                          
-              <FaMapMarkerAlt />
-                          <span>Av. Jorge Luis Borges 25</span>          
-            </p>
-                      
-            <p>
-                          
-              <FaPhoneAlt />
-                          <span>63454643 / 34534443</span>          
-            </p>
-                      
-            <p>
-                          
-              <FaClock />
-                          
-              <span>
-                              Lunes a Sábado de 10h a 20h <br />
-                              Domingo Cerrado            
-              </span>
-                        
-            </p>
-                    
-          </div>
-                  {/* Acciones */}        
-          <div className="footer__actions">
-                      
-            <button className="footer__button">
-                          Reservar una cita          
-            </button>
-                      
-            <div className="footer__social">
-                          
-              <a href="#" aria-label="Instagram">
-                              
-                <FaInstagram />
-                            
-              </a>
-                          
-              <a href="#" aria-label="WhatsApp">
-                              
-                <FaWhatsapp />
-                            
-              </a>
-                          
-              <a href="#" aria-label="Email">
-                              
-                <FaEnvelope />
-                            
-              </a>
-                        
-            </div>
-                    
-          </div>
-                
+    <footer className="footer">
+      <div className="footer__content">
+        {/* Logo */}
+        <div className="footer__logo">
+          <img src="/logo-bruver.png" alt="Brüver - Fieles a tu mascota" />
         </div>
-              {/* Copyright */}      
-        <div className="footer__bottom">
-                  © 2026 Brüver. Reservados todos los derechos      
+
+        {/* Información */}
+        <div className="footer__info">
+          <h4>Dónde estamos</h4>
+          <p>
+            <FaMapMarkerAlt />
+            <span>Av. Jorge Luis Borges 25</span>
+          </p>
+          <p>
+            <FaPhoneAlt />
+            <span>63454643 / 34534443</span>
+          </p>
+          <p>
+            <FaClock />
+            <span>
+              Lunes a Sábado de 10h a 20h <br />
+              Domingo Cerrado
+            </span>
+          </p>
         </div>
-            
-      </footer>
-    </div>
+
+        {/* Acciones */}
+        <div className="footer__actions">
+          <button className="footer__button">Reservar una cita</button>
+          <div className="footer__social">
+            <a href="#" aria-label="Instagram">
+              <FaInstagram />
+            </a>
+            <a href="#" aria-label="WhatsApp">
+              <FaWhatsapp />
+            </a>
+            <a href="#" aria-label="Email">
+              <FaEnvelope />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Copyright */}
+      <div className="footer__bottom">
+        © 2026 Brüver. Reservados todos los derechos
+      </div>
+    </footer>
   );
 };
 
