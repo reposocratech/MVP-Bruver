85e49fb612b4bb7843513ef210bc18b7d0f3fa16
Ysujpg <sakurasleavess@gmail.com>
2026-01-23 14:28:44 +0100

    Design

--- client/src/pages/PublicPages/Pharmacy/Pharmacy.jsx
+++ client/src/pages/PublicPages/Pharmacy/Pharmacy.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import "./pharmacy.css";
-
+ 
 const Pharmacy = () => {
   return (
     <section className="farmaciaPage">
       <section className="farmaciaContent">
         <h1 className="farmaciaTitle">Farmacia Veterinaria</h1>
         <h2 className="farmaciaTitle">Próximamente</h2>
-
+ 
    
-
+ 
         <p className="farmaciaText">
           En nuestra farmacia veterinaria te ayudamos a cuidar de tu mascota con
           el mismo cariño con el que lo harías tú. Te asesoramos de forma
           personalizada para encontrar el tratamiento más adecuado, resolver
           dudas sobre medicación y elegir los mejores productos para su bienestar
           diario.
+         <br></br>
+         <br></br>
+          ¿Tienes alguna duda? Haz clic en la imagen y rellena el formulario. Estaremos encantados de ayudarte.
         </p>
-
+ 
         <img
           className="farmaciaImg"
           src="/img/pharmacy/pharmacy.png"
           alt="Farmacia Veterinaria"
         />
+
+        
       </section>
-      
+     
     </section>
   );
 };
 export default Pharmacy
+
