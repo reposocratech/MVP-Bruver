1855f406596fd17624c06e3d15f9412301624ce0
Javier Sanz <javiersanzvilloldo@gmail.com>
2026-01-23 13:40:15 +0100

    funcion nodemailer y .env

--- client/src/layouts/PublicLayout.jsx
+++ client/src/layouts/PublicLayout.jsx
@@ -1,16 +1,12 @@
 import { Outlet } from "react-router";
 import { NavbarPublic } from "../components/Navbar/NavbarPublic";
 import { FooterPublic } from "../components/Footer/FooterPublic";
-import { NavbarLogin } from "../components/NavbarLogin/NavbarLogin";
-
 
 export const PublicLayout = () => {
   return (
     <>
       <header>
         <NavbarPublic />
-        <br /><br />
-        <NavbarLogin />
       </header>
       <main>
          <Outlet />
--- server/modules/user/user.controller.js
+++ server/modules/user/user.controller.js
@@ -1,3 +1,4 @@
+import { sendEmail } from '../../services/emailService.js';
 import userDal from './user.dal.js';
 import bcrypt from "bcrypt";
 
@@ -8,7 +9,17 @@ class UserController {
         
         let hashedPass = await bcrypt.hash(password, 10);
         let result = await userDal.register([name_user, last_name || null, phone, email, province || null, city || null, hashedPass]);
-        res.status(200).json(result);
+        
+        const html = `<h1>Bienvenido a Brüver</h1>
+        <p>Hola ${name_user}, tu registro ha sido completado exitosamente.</p>`;
+        
+            try {
+             await sendEmail(email, html);
+             } catch (err) {
+              console.error("Error al enviar email de bienvenida:", err);
+              }
+             res.status(200).json(result);
+        
       } catch (error) {
         console.log(error);
         res.status(500).json(error);
