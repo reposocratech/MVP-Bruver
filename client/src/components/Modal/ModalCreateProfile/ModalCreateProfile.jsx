22afd7b98ce8e65555c38ba070c1cdd50e560005
JuancokTrj <juancarlostorreswork@gmail.com>
2026-02-04 12:55:45 +0100

    css y mejoras

--- client/src/components/Modal/ModalCreateProfile/ModalCreateProfile.jsx
+++ client/src/components/Modal/ModalCreateProfile/ModalCreateProfile.jsx
@@ -1,6 +1,4 @@
-import { Modal, Form } from 'react-bootstrap';
 import './ModalCreateProfile.css';
-
 
 const ModalCreateProfile = ({
   show,
@@ -11,174 +9,123 @@ const ModalCreateProfile = ({
   valErrors,
   fetchError,
 }) => {
+  if (!show) return null;
+
   return (
-    <>
-      {show && (
-        <section>
-          <div className='createProfileGridModal'>
-        <Modal
-          show={show}
-          /* onHide={onClose} */
-          centered
-          dialogClassName="nc-modal-dialog"
-          contentClassName="nc-modal-content"
-          backdropClassName="nc-backdrop"
-        >
-          <Modal.Body className="createProfileCardModal">
-            <h3 className="nc-title">Nuevo perfil</h3>
+    <section className="nc-overlay" onClick={() => onClose(false)}>
+      <div className="nc-modal" onClick={(e) => e.stopPropagation()}>
+        <h3 className="nc-title">Nuevo perfil</h3>
 
-            {fetchError && <div className="error-msg">{fetchError}</div>}
+        {fetchError && <div className="error-msg">{fetchError}</div>}
 
-            <Form className="nc-form formNewProfile" onSubmit={submitProfile}>
-              <Form.Group className="nc-group">
-                <Form.Label>Tipo de perfil</Form.Label>
-                <Form.Select
-                  name="type"
-                  value={newProfile.type}
-                  onChange={handleProfileChange}
-                >
-                  <option value="">Elige</option>
-                  <option value="worker">Trabajador</option>
-                  <option value="client">Cliente</option>
-                </Form.Select>
-                {valErrors?.type && <div className="error-msg">{valErrors.type}</div>}
-              </Form.Group>
+        <form className="nc-form" onSubmit={submitProfile}>
+          <div className="nc-group">
+            <label>Tipo de perfil</label>
+            <select name="type" value={newProfile.type} onChange={handleProfileChange}>
+              <option value="">Elige</option>
+              <option value="worker">Trabajador</option>
+              <option value="client">Cliente</option>
+            </select>
+            {valErrors?.type && <div className="error-msg">{valErrors.type}</div>}
+          </div>
 
+          <div className="nc-group">
+            <label>Nombre*</label>
+            <input
+              name="name_user"
+              value={newProfile.name_user}
+              onChange={handleProfileChange}
+              type="text"
+            />
+            {valErrors?.name_user && <div className="error-msg">{valErrors.name_user}</div>}
+          </div>
 
-              <Form.Group className="nc-group">
-                <Form.Label>Nombre*</Form.Label>
-                <Form.Control
-                  name="name_user"
-                  value={newProfile.name_user}
-                  onChange={handleProfileChange}
-                  type="text"
-                />
-                {valErrors?.name_user && <div className="error-msg">{valErrors.name_user}</div>}
-              </Form.Group>
+          <div className="nc-group">
+            <label>Apellidos</label>
+            <input
+              name="last_name"
+              value={newProfile.last_name}
+              onChange={handleProfileChange}
+              type="text"
+            />
+          </div>
 
-              <Form.Group className="nc-group">
-                <Form.Label>Apellidos</Form.Label>
-                <Form.Control
-                  name="last_name"
-                  value={newProfile.last_name}
-                  onChange={handleProfileChange}
-                  type="text"
-                />
-              </Form.Group>
+          <div className="nc-group">
+            <label>Teléfono</label>
+            <input
+              name="phone"
+              value={newProfile.phone}
+              onChange={handleProfileChange}
+              type="text"
+            />
+          </div>
 
-              <Form.Group className="nc-group">
-                <Form.Label>Teléfono</Form.Label>
-                <Form.Control
-                  name="phone"
-                  value={newProfile.phone}
-                  onChange={handleProfileChange}
-                  type="text"
-                />
-              </Form.Group>
+          <div className="nc-group">
+            <label>Email*</label>
+            <input
+              name="email"
+              value={newProfile.email}
+              onChange={handleProfileChange}
+              type="email"
+            />
+            {valErrors?.email && <div className="error-msg">{valErrors.email}</div>}
+          </div>
 
+          <div className="nc-group">
+            <label>Contraseña</label>
+            <input
+              name="password"
+              value={newProfile.password}
+              onChange={handleProfileChange}
+              type="password"
+            />
+            {valErrors?.password && <div className="error-msg">{valErrors.password}</div>}
+          </div>
 
-              <Form.Group className="nc-group">
-                <Form.Label>Email*</Form.Label>
-                <Form.Control
-                  name="email"
-                  value={newProfile.email}
-                  onChange={handleProfileChange}
-                  type="email"
-                />
-                {valErrors?.email && <div className="error-msg">{valErrors.email}</div>}
-              </Form.Group>
+          <div className="nc-group">
+            <label>Provincia</label>
+            <input
+              name="province"
+              value={newProfile.province}
+              onChange={handleProfileChange}
+              type="text"
+            />
+          </div>
 
+          <div className="nc-group">
+            <label>Ciudad</label>
+            <input
+              name="city"
+              value={newProfile.city}
+              onChange={handleProfileChange}
+              type="text"
+            />
+          </div>
 
-             <Form.Group className="nc-group">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control
-                  name="password"
-                  value={newProfile.password}
-                  onChange={handleProfileChange}
-                  type="password"
-                />
-                {valErrors?.password && <div className="error-msg">{valErrors.password}</div>}
-              </Form.Group> 
+          <div className="nc-group">
+            <label>Dirección</label>
+            <input
+              name="address"
+              value={newProfile.address}
+              onChange={handleProfileChange}
+              type="text"
+            />
+          </div>
 
+          <div className="nc-buttons">
+            <button className="btn-accept" type="submit">
+              ACEPTAR
+            </button>
 
-              <Form.Group className="nc-group">
-                <Form.Label>Provincia</Form.Label>
-                <Form.Control
-                  name="province"
-                  value={newProfile.province}
-                  onChange={handleProfileChange}
-                  type="text"
-                />
-              </Form.Group>
+            <button className="btn-cancel" type="button" onClick={() => onClose(false)}>
+              CANCELAR
+            </button>
+          </div>
 
-              <Form.Group className="nc-group">
-                <Form.Label>Ciudad</Form.Label>
-                <Form.Control
-                  name="city"
-                  value={newProfile.city}
-                  onChange={handleProfileChange}
-                  type="text"
-                />
-              </Form.Group>
-
-              <Form.Group className="nc-group">
-                <Form.Label>Dirección</Form.Label>
-                <Form.Control
-                  name="address"
-                  value={newProfile.address }
-                  onChange={handleProfileChange}
-                  type="text"
-                />
-              </Form.Group>
-
-{/*               <Form.Group className="nc-group">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control
-                  name="password"
-                  value={newProfile.password}
-                  onChange={handleProfileChange}
-                  type="password"
-                />
-              </Form.Group>
-
-              <Form.Group className="nc-group">
-                <Form.Label>Repetir contraseña</Form.Label>
-                <Form.Control
-                  name="repeatPassword"
-                  value={newProfile.repeatPassword}
-                  onChange={handleProfileChange}
-                  type="password"
-                />
-              </Form.Group> */}
-
-
-              <div className="nc-buttons">
-                <button className="close" type="submit">
-                  ACEPTAR
-                </button>
-                <button
-                  className="close"
-                  type="button"
-                  onClick={() => onClose(false)}
-                >
-                  CANCELAR
-                </button>
-              </div>
-
-              <p className="nc-slogan">Patitas limpias, corazones felices</p>
-
-              <img
-                className="nc-dog"
-                src="/img/adminAddUser/dog1.png"
-                alt="dog"
-              />
-            </Form>
-          </Modal.Body>
-        </Modal>
-        </div>
-        </section>
-      )}
-    </>
+          <p className="nc-slogan">Patitas limpias, corazones felices</p>
+        </form>
+      </div>
+    </section>
   );
 };
 
